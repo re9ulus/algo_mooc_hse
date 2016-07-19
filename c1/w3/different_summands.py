d891d8536d3db58eede728fcf54445d8f3a6fd41
Vasiliy Zemlyanov <angeldaren@gmail.com>
2016-07-20 00:19:02 +0300

    Add solution to last problem of 3 week

--- c1/w3/different_summands.py
+++ c1/w3/different_summands.py
@@ -1,9 +1,18 @@
-# Uses python3
+# Uses python2
 import sys
 
 def optimal_summands(n):
     summands = []
     #write your code here
+    i = 1
+    while n > 0:
+    	if n - i <= i:
+    		summands.append(n)
+    		break
+    	else:
+    		summands.append(i)
+    		n -= i
+    		i += 1
     return summands
 
 if __name__ == '__main__':
@@ -11,5 +20,4 @@ if __name__ == '__main__':
     n = int(input)
     summands = optimal_summands(n)
     print(len(summands))
-    for x in summands:
-        print(x, end=' ')
+    print(' '.join(map(str, summands)))
